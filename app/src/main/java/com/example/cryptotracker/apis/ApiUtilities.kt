0351e0801e07036df0194d44972033b70c740a1b
Ronin@73 <raghuvanshiatul627@gmail.com>
2023-09-27 21:22:47 +0530

    updated API uitilities

--- app/src/main/java/com/example/cryptotracker/apis/ApiUtilities.kt
+++ app/src/main/java/com/example/cryptotracker/apis/ApiUtilities.kt
@@ -1,6 +1,14 @@
 package com.example.cryptotracker.apis
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiUtilities {
 
-    fun getInstance() :
+    fun getInstance() : Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://api.coinmarketcap.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
