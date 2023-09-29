77fe2778a66cbc473726d28a9721b396aa267bf5
Ronin@73 <raghuvanshiatul627@gmail.com>
2023-09-29 13:27:30 +0530

    adding a adapter file

---
+++ app/src/main/java/com/example/cryptotracker/adapter/TopLossGainPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.cryptotracker.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class TopLossGainPagerAdapter(fragment : Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+
+}
