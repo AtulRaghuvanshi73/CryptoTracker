a795801173bf14f9c595384736a8f0e850c9c768
Ronin@73 <raghuvanshiatul627@gmail.com>
2023-09-30 00:16:49 +0530

    updated application

--- app/src/main/java/com/example/cryptotracker/adapter/MarketAdapter.kt
+++ app/src/main/java/com/example/cryptotracker/adapter/MarketAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.cryptotracker.R
 import com.example.cryptotracker.databinding.CurrencyItemLayoutBinding
+import com.example.cryptotracker.fragment.HomeFragment
+import com.example.cryptotracker.fragment.HomeFragmentDirections
 import com.example.cryptotracker.models.CryptoCurrency
 
 class MarketAdapter(var context: Context, var list : List<CryptoCurrency>) : RecyclerView.Adapter<MarketAdapter.MarketViewHolder>() {
@@ -24,6 +27,7 @@ class MarketAdapter(var context: Context, var list : List<CryptoCurrency>) : Rec
         val item = list[position]
         holder.binding.currencyNameTextView.text = item.name
         holder.binding.currencySymbolTextView.text = item.symbol
+
         Glide.with(context).load(
             "https://s2.coinmarketcap.com/static/img/coins/64x64/" + item.id + ".png"
         ).thumbnail(Glide.with(context).load(R.drawable.spinner))
@@ -35,6 +39,8 @@ class MarketAdapter(var context: Context, var list : List<CryptoCurrency>) : Rec
             .into(holder.binding.currencyChartImageView)
 
         holder.binding.currencyPriceTextView.text = "${String.format("$%.02f", item.quotes[0].price)}"
+
+
         if (item.quotes!![0].percentChange24h > 0){
 
             holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
@@ -42,6 +48,12 @@ class MarketAdapter(var context: Context, var list : List<CryptoCurrency>) : Rec
         }else{
             holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.red))
             holder.binding.currencyChangeTextView.text = "${String.format("%.02f", item.quotes[0].percentChange24h)}%"
+        }
+
+        holder.itemView.setOnClickListener{
+            findNavController(it).navigate(
+                HomeFragmentDirections.actionHomeFragmentToDetialsFragment(item)
+            )
         }
     }
 
