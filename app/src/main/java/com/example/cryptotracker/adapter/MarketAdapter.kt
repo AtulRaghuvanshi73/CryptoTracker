f9814975b96b2e4360d4224f4ddea31395a530e0
Ronin@73 <raghuvanshiatul627@gmail.com>
2023-09-29 19:54:42 +0530

    home page completed

--- app/src/main/java/com/example/cryptotracker/adapter/MarketAdapter.kt
+++ app/src/main/java/com/example/cryptotracker/adapter/MarketAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.cryptotracker.R
 import com.example.cryptotracker.databinding.CurrencyItemLayoutBinding
 import com.example.cryptotracker.models.CryptoCurrency
@@ -20,10 +21,31 @@ class MarketAdapter(var context: Context, var list : List<CryptoCurrency>) : Rec
     }
 
     override fun onBindViewHolder(holder: MarketViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = list[position]
+        holder.binding.currencyNameTextView.text = item.name
+        holder.binding.currencySymbolTextView.text = item.symbol
+        Glide.with(context).load(
+            "https://s2.coinmarketcap.com/static/img/coins/64x64/" + item.id + ".png"
+        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.binding.currencyImageView)
+
+        Glide.with(context).load(
+            "https://s3.coinmarketcap.com/generated/sparklines/web/7d/usd/" + item.id + ".png"
+        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.binding.currencyChartImageView)
+
+        holder.binding.currencyPriceTextView.text = "${String.format("$%.02f", item.quotes[0].price)}"
+        if (item.quotes!![0].percentChange24h > 0){
+
+            holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
+            holder.binding.currencyChangeTextView.text = "+ ${String.format("%.02f", item.quotes[0].percentChange24h)}%"
+        }else{
+            holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.red))
+            holder.binding.currencyChangeTextView.text = "${String.format("%.02f", item.quotes[0].percentChange24h)}%"
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 }
